8de2919fbbaede2815ae5c3c3f77cd2164e42544
Luwei Ge <lwge@google.com>
2020-08-27 00:19:01 +0000

    typo fix in opdk script

--- tests/test_opdk.py
+++ tests/test_opdk.py
@@ -46,9 +46,9 @@ def main():
 
   utils.provision(logger, apigee_client)
 
-  os.environ["ENVOY_CONFIG"] = os.getenv("PWD") + "/legacy-envoy/envoy-config.yaml"
+  os.environ["ENVOY_CONFIG"] = os.getenv("PWD") + "/opdk-envoy/envoy-config.yaml"
   cmd = [os.getenv("CLI_DIR", ".")+"/apigee-remote-service-cli", "samples", "create",
-          "-c", "config.yaml", "--out", "opdk-envoy", "-t", "native", "-f"]
+          "-c", "config.yaml", "--out", "opdk-envoy", "-t", "native"]
   process = subprocess.run(cmd, capture_output=True)
   if process.stderr != b'':
     logger.error(process.stderr.decode())
@@ -65,31 +65,28 @@ def main():
 
   utils.stop_containers(logger)
 
-  if os.getenv("K8S_CONTEXT", "") != "":
-    time.sleep(3)
+  # if os.getenv("K8S_CONTEXT", "") != "":
+  #   time.sleep(3)
 
-    logger.info("starting istio tests envoy as sidecars")
+  #   logger.info("starting istio tests envoy as sidecars")
     
-    cmd = ["kubectl", "config", "use-context", os.getenv("K8S_CONTEXT")]
-    process = subprocess.run(cmd, capture_output=True)
-    if process.stderr != b'':
-      logger.error(process.stderr.decode())
-    logger.debug(process.stdout.decode())
+  #   cmd = ["kubectl", "config", "use-context", os.getenv("K8S_CONTEXT")]
+  #   process = subprocess.run(cmd, capture_output=True)
+  #   if process.stderr != b'':
+  #     logger.error(process.stderr.decode())
+  #   logger.debug(process.stdout.decode())
 
-    deploy(logger)
+  #   deploy(logger)
 
-    logger.debug("waiting for adapter to fetch the latest product info. this takes about two minutes...")
-    time.sleep(140)
+  #   utils.start_istio_test(logger, apigee_client)
 
-    utils.start_istio_test(logger, apigee_client)
-
-    deployment.KubeManager.delete("apigee-remote-service-envoy", "apigee", logger)
-    deployment.KubeManager.delete("apigee-remote-service-envoy", "apigee", logger, "services")
-    deployment.KubeManager.delete("curl", "default", logger, "pods")
-    deployment.KubeManager.delete("httpbin", "default", logger)
-    deployment.KubeManager.delete("httpbin", "default", logger, "services")
-    deployment.KubeManager.delete("apigee", "default", logger, "requestauthentications.security.istio.io")
-    deployment.KubeManager.delete("apigee-remote-httpbin", "default", logger, "envoyfilters.networking.istio.io")
+  #   deployment.KubeManager.delete("apigee-remote-service-envoy", "apigee", logger)
+  #   deployment.KubeManager.delete("apigee-remote-service-envoy", "apigee", logger, "services")
+  #   deployment.KubeManager.delete("curl", "default", logger, "pods")
+  #   deployment.KubeManager.delete("httpbin", "default", logger)
+  #   deployment.KubeManager.delete("httpbin", "default", logger, "services")
+  #   deployment.KubeManager.delete("apigee", "default", logger, "requestauthentications.security.istio.io")
+  #   deployment.KubeManager.delete("apigee-remote-httpbin", "default", logger, "envoyfilters.networking.istio.io")
 
   utils.cleanup(logger, apigee_client)
 
