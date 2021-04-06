5ae4d30582072c98f5147a6820acb7e7012b02da
Luwei Ge <lwge@google.com>
2021-04-06 19:14:22 +0000

    add package comment

--- arcconfig/arcconfig.go
+++ arcconfig/arcconfig.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package arcconfig defines the Apigee Runtime Control config and provides
+// the config loading and validation functions.
 package arcconfig
 
 import "time"
